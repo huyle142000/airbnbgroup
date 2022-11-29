2246a153ac288cbce8a46936dc8bc9366d019354
SneakyTurtle <le741996cf@gmail.com>
2022-11-29 21:36:11 +0700

    add style of _reset.scss

--- src/App.js
+++ src/App.js
@@ -3,30 +3,32 @@ import Home from "./pages/Home/Home";
 import "antd/dist/antd.min.css";
 import AdminTemplate from "./Templates/AdminTemplate/AdminTemplate";
 import User from "./pages/Admin/UserAdmin/User";
-import EditUser from "./pages/Admin/UserAdmin/EditUser/EditUser";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import EditUser from "./pages/Admin/UserAdmin/EditUser/EditUser";
+import "./assets/sass/main.scss";
+
 function App() {
-  return (
-    <BrowserRouter>
-      <ToastContainer />
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="*" element={<Navigate to="" />} />
-        {/* ************* Admin ************ */}
-        <Route element={<AdminTemplate />}>
-          <Route path="/admin" element={<User />} />
-          <Route path="/admin/userlist" element={<User />} />
-          <Route path="/admin/edituser" element={<EditUser />}>
-            <Route path=":id" element={<EditUser />} />
-          </Route>
-          <Route path="/admin/listroom" element={<User />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <ToastContainer />
+            <Routes>
+                <Route path="" element={<Home />} />
+                <Route index element={<Home />} />
+                <Route path="home" element={<Home />} />
+                <Route path="*" element={<Navigate to="" />} />
+                {/* ************* Admin ************ */}
+                <Route element={<AdminTemplate />}>
+                    <Route path="/admin" element={<User />} />
+                    <Route path="/admin/userlist" element={<User />} />
+                    <Route path="/admin/edituser" element={<EditUser />}>
+                        <Route path=":id" element={<EditUser />} />
+                    </Route>
+                    <Route path="/admin/listroom" element={<User />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return <div className="container header">
+      <div className="header__top">
+        <h1>Demo</h1>
+      </div>
+    </div>;
+}
